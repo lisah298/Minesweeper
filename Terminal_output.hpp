ab937128dbf4ebd6cd4a0db997993e432dadbd41
lisa <lisa.hetzel@jh-webworks.de>
2022-01-28 16:46:00 +0100

    deleted functions moved to terminalprinter

--- Terminal_output.hpp
+++ Terminal_output.hpp
@@ -9,65 +9,6 @@ class Grid
 {
 private:
     std::vector<std::vector<Tile>> tiles;
-    string decorate_tile(const Tile &tile)
-    {
-        switch (tile)
-        {
-        case '1':
-            return "\033[38;5;4m"
-                   " 1 "
-                   "\033[0m";
-        case '2':
-            return "\033[38;5;22m"
-                   " 2 "
-                   "\033[0m";
-        case '3':
-            return "\033[38;5;124m"
-                   " 3 "
-                   "\033[0m";
-        case '4':
-            return "\033[38;5;17m"
-                   " 4 "
-                   "\033[0m";
-        case '5':
-            return "\033[38;5;52m"
-                   " 5 "
-                   "\033[0m";
-        case '6':
-            return "\033[38;5;29m"
-                   " 6 "
-                   "\033[0m";
-        case '7':
-            return "\033[38;5;54m"
-                   " 7 "
-                   "\033[0m";
-        case '8':
-            return "\033[38;5;8m"
-                   " 8 "
-                   "\033[0m";
-        case '@':
-            return "\033[48;5;243m"
-                   "   "
-                   "\033[0m";
-        case 'A':
-            return "\033[48;5;202m"
-                   "   "
-                   "\033[0m";
-        case '*':
-            return "\033[48;5;124m"
-                   "   "
-                   "\033[0m";
-        case 'L':
-            return "\033[48;5;196m"
-                   "   "
-                   "\033[0m";
-
-        case '0':
-            return "\033[48;5;15m"
-                   "   "
-                   "\033[0m";
-        }
-    };
 
 public:
     Grid(const GridSize &size) : tiles(size.rows, std::vector<Tile>(size.columns, '@')){
@@ -93,40 +34,6 @@ public:
     {
         return tiles[row][column];
     }
-
-    void print(const Grid &grid, const Metadata &metadata)
-    {
-        size_t cols = metadata.grid_size.columns;
-        //printe erste Zeile mit Spaltennummern
-        cout
-            << "    ";
-        for (int i = 0; i < metadata.grid_size.columns; ++i)
-        { //number with 3 spaces
-            cout << i << "   ";
-        }
-        cout << endl;
-        cout << "   " << string(4 * cols, '=') << endl;
-        for (size_t row = 0; row < metadata.grid_size.rows; ++row)
-        {
-            // print row at beginning
-            cout << row << " | ";
-            for (size_t column = 0; column < metadata.grid_size.columns; ++column)
-            {
-                cout << decorate_tile(grid(row, column));
-                cout << "|";
-            }
-
-            cout << ' ' << row << endl;
-            cout << "   " << string(4 * cols, '=') << endl; // neue Zeile mit -
-        }
-        cout
-            << "    ";
-        for (int i = 0; i < metadata.grid_size.columns; ++i)
-        { //number with 3 spaces
-            cout << i << "   ";
-        };
-        cout << '\n';
-    }
 };
 
 class TerminalPrinter
@@ -148,7 +55,7 @@ public:
                 cout << "|";
             }
 
-            cout << endl;
+            cout << ' ' << row << endl;
             print_line_of_equals(cols);
         }
         print_numbers(cols);
