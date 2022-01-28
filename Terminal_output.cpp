a814530d0e08b11c7edc3192713990f2b3104a29
lisa <lisa.hetzel@jh-webworks.de>
2022-01-28 16:44:16 +0100

    call terminalprinter class

--- Terminal_output.cpp
+++ Terminal_output.cpp
@@ -6,5 +6,6 @@ int main()
     GridSize gridsize{3, 8};
     Metadata metadata{gridsize, 2};
     Grid grid{gridsize};
-    grid.print(grid, metadata);
+    TerminalPrinter t{};
+    t.print(grid, metadata);
 };
