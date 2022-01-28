b335a6ed2291b7c667b1022edebf5c7606969cbe
lisa <lisa.hetzel@jh-webworks.de>
2022-01-28 17:21:44 +0100

    added class member

--- Command.hpp
+++ Command.hpp
@@ -1,6 +1,17 @@
+#include <memory>
+
 using Action = char;
+using namespace std;
 class Command
 {
-public:
+
 private:
+    Action action;
+    unique_ptr<Coordinates> coordinates;
+
+public:
+    const Action &get_action() const { return action; };
+    const Coordinates &get_coordinates() const
+    {
+        }
 };
