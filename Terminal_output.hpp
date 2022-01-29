9bfe1d934bf46810396bb8af1ab9cfa3f9cfa48b
lisa <lisa.hetzel@jh-webworks.de>
2022-01-29 11:46:15 +0100

    moved grid class to extra file

--- Terminal_output.hpp
+++ Terminal_output.hpp
@@ -1,40 +1,12 @@
 #include "Types.hpp"
+#include "Grid.hpp"
 #include <vector>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-class Grid
-{
-private:
-    std::vector<std::vector<Tile>> tiles;
-
-public:
-    Grid(const GridSize &size) : tiles(size.rows, std::vector<Tile>(size.columns, 'W')){
-                                     // Teste, ob rows und columns richtig übergeben werden
-                                     //cout << "rows: " << size.rows << " columns: " << size.columns << endl;
-                                     //cout << grey << endl;
-                                 };
-
-    const Tile &operator()(Coordinates coords) const
-    {
-        return tiles[coords.x][coords.y];
-    };
-
-    Tile &operator()(Coordinates coords)
-    {
-        return tiles[coords.x][coords.y];
-    };
-    const Tile &operator()(size_t row, size_t column) const
-    {
-        return tiles.at(row).at(column);
-    }
-    Tile &operator()(size_t row, size_t column)
-    {
-        return tiles[row][column];
-    }
-};
+class Grid;
 
 class TerminalPrinter
 {
