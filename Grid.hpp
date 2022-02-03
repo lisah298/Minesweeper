c17238025b1620197987ab915457783c6e7143cc
lisa <lisa.hetzel@jh-webworks.de>
2022-02-03 20:26:45 +0100

    added virtual destructor and private to protected

--- Grid.hpp
+++ Grid.hpp
@@ -9,15 +9,17 @@ using namespace std;
 
 class Grid
 {
-private:
+protected:
     std::vector<std::vector<Tile>> tiles;
     size_t nRows() const { return tiles.size(); };
     size_t nCols() const { return tiles[0].size(); };
 
 public:
     explicit Grid(const GridSize &size) : tiles(size.rows, std::vector<Tile>(size.columns, '-')){};
+    virtual ~Grid(){};
     GridSize size() const { return {nRows(), nCols()}; }
-    const Tile &operator()(Coordinates coords) const
+    const Tile &
+    operator()(Coordinates coords) const
     {
         return tiles[coords.x][coords.y];
     };
@@ -29,9 +31,9 @@ public:
     const Tile &operator()(size_t row, size_t column) const
     {
         return tiles.at(row).at(column);
-    }
+    };
     Tile &operator()(size_t row, size_t column)
     {
         return tiles[row][column];
-    }
+    };
 };
